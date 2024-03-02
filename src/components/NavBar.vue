ec35675a5cad43303def8c7f29223bcd55b4a089
Jhony Freitas <jhony00._@hotmail.com>
2024-03-02 11:16:20 -0300

    Criando NavBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,41 @@
+<template>
+    <header>
+        <nav>
+            <ul class="list">
+                <li class="item">
+                    <router-link to='/clients'>Cadastro de clientes</router-link>
+                </li>
+                <li class="item">
+                    <router-link to='/products'>Cadastro de produtos</router-link>
+                </li>
+                <li class="item">
+                    <router-link to='/association'>Associação de produtos com clientes</router-link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'nav-bar',
+}
+</script>
+
+<style scoped>
+header {
+    width: 100%;
+}
+
+header .list {
+    display: flex;
+    justify-content: center;
+    gap: 25px;
+    background-color: lightgray;
+    padding: 10px;
+}
+
+header .list .item a {
+    font-size: 20px;
+}
+</style>
