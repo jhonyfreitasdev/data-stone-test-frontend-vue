8d28ec950f1c06fb77e3b4afaa95c9c99313fafa
Jhony Freitas <jhony00._@hotmail.com>
2024-03-02 11:18:35 -0300

    Adicionando o Router view no App

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,39 @@
-<script setup></script>
-
 <template>
-	<div>
+	<div class="app-container">
+		<NavBar />
+		<main>
+			<router-view />
+		</main>
 	</div>
 </template>
+
+<script>
+import NavBar from './components/NavBar.vue';
+
+export default {
+	name: 'app',
+	components: {
+		NavBar
+	}
+}
+</script>
+
+<style scoped>
+.app-container {
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	gap: 45px;
+	align-items: center;
+	max-width: 1440px;
+	height: 100vh;
+	margin: 0 auto;
+}
+
+.app-container main {
+	background-color: lightgray;
+	width: 80%;
+	border-radius: 8px;
+	padding: 20px;
+}
+</style>
