b59f94050d9ccb310159e276b1255e10ba6951b1
Jhony Freitas <jhony00._@hotmail.com>
2024-03-02 11:19:18 -0300

    Criando página de clientes

---
+++ src/views/Clients.vue
@@ -0,0 +1,14 @@
+<template>
+    <ClientRegistrationForm />
+</template>
+
+<script>
+import ClientRegistrationForm from '../components/form/ClientRegistrationForm.vue';
+
+export default {
+    name: 'clients',
+    components: {
+        ClientRegistrationForm
+    }
+}
+</script>
