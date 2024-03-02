529f48f232d50332a870dbd9862e7eec70540a3c
Jhony Freitas <jhony00._@hotmail.com>
2024-03-02 18:48:13 -0300

    Criando página de associate customer product

---
+++ src/views/AssociateCustomerProduct.vue
@@ -0,0 +1,14 @@
+<template>
+    <AssociationForm />
+</template>
+
+<script>
+import AssociationForm from '../components/form/AssociationForm.vue';
+
+export default {
+    name: 'associate-customer-product',
+    components: {
+        AssociationForm
+    }
+}
+</script>
