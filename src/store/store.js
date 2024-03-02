60b2f826d82e8e6fbaf52ac1852d458847aa31ff
Jhony Freitas <jhony00._@hotmail.com>
2024-03-02 14:06:39 -0300

    Adicionando mutation addProducts

--- src/store/store.js
+++ src/store/store.js
@@ -4,10 +4,13 @@ const store = createStore({
     state: {
         clients: [],
         products: []
-    }, 
+    },
     mutations: {
         addClient(state, client) {
             state.clients.push(client);
+        },
+        addProduct(state, product) {
+            state.products.push(product);
         }
     }
 });
