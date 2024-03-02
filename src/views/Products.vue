cc63b2e540c6b434980c2f121b1fcf3c3cc14026
Jhony Freitas <jhony00._@hotmail.com>
2024-03-02 11:19:03 -0300

    Criando página de Produtos

---
+++ src/views/Products.vue
@@ -0,0 +1,14 @@
+<template>
+    <ProductRegistrationForm />
+</template>
+
+<script>
+import ProductRegistrationForm from '../components/form/ProductRegistrationForm.vue';
+
+export default {
+    name: 'products',
+    components: {
+        ProductRegistrationForm
+    }
+}
+</script>
