1db521a53d2bd2bc357f5b6178b6b7bc44e03f86
Jhony Freitas <jhony00._@hotmail.com>
2024-03-02 18:49:12 -0300

    Renomeando Página

---
+++ src/views/Customer.vue
@@ -0,0 +1,14 @@
+<template>
+    <CustomerRegistrationForm />
+</template>
+
+<script>
+import CustomerRegistrationForm from '../components/form/CustomerRegistrationForm.vue';
+
+export default {
+    name: 'customer',
+    components: {
+        CustomerRegistrationForm
+    }
+}
+</script>
