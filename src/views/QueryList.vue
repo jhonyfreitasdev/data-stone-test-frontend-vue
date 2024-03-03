684a9a65295da6d839ad2acf41358a55673b5081
Jhony Freitas <jhony00._@hotmail.com>
2024-03-03 17:07:43 -0300

    Optimizando view só para visualização

--- src/views/QueryList.vue
+++ src/views/QueryList.vue
@@ -7,95 +7,35 @@
             <button @click="changeList" type="button" id="products">Lista de produtos</button>
         </div>
 
-        <div v-if="selectedList === 'customer'" class="customer-list">
-            <p>Lista de Clientes:</p>
-
-            <ul class="list">
-                <li class="item" v-for="customer in customerList" :key="customer.document">
-
-                </li>
-            </ul>
+        <div v-if="selectedList === 'customer'">
+            <CustomerList />
         </div>
 
-        <div v-else-if="selectedList === 'products'" class="product-list">
-            <p>Lista de Produtos:</p>
-
-            <ul class="list">
-                <li class="item" v-for="product in productList" :key="product.name">
-                    <div class="content" v-if="fieldInEditing !== product.name">
-                        <p>Nome: {{ product.name }}</p>
-                        <p>Ativo: {{ product.activatedStatus ? 'Sim' : 'Não' }}</p>
-                        <button @click="editField(product)" type="button"> Editar </button>
-                        <button @click="removeItem(product)" type="button"> Remover </button>
-                    </div>
-
-                    <div class="content edit" v-else-if="fieldInEditing === product.name">
-                        <div class="input-container">
-                            <label for="name-edit">Nome:</label>
-                            <input v-model="editedProducts.nameEdited" id="name-edit" name="name" type="text" />
-                        </div>
-                        <div class="input-container">
-                            <label for="status-edit">Ativo:</label>
-                            <select v-model="editedProducts.statusEdited" id="status-edit" name="status">
-                                <option value="yes"> Sim </option>
-                                <option value="no"> Não </option>
-                            </select>
-                        </div>
-                        <div class="input-container">
-                            <button @click="cancelEdit" class="btn-cancel" type="button"> Cancelar </button>
-                            <button @click="saveField(product)" class="btn-save" type="button"> Salvar </button>
-                        </div>
-                    </div>
-                </li>
-            </ul>
+        <div v-else-if="selectedList === 'products'">
+            <ProductList />
         </div>
     </div>
 </template>
 
 <script>
+import CustomerList from '../components/list/CustomerList.vue';
+import ProductList from '../components/list/ProductList.vue';
+
 export default {
     name: 'query-list',
     data() {
         return {
             selectedList: 'customer',
-            customerList: this.$store.state.association || [],
-            productList: this.$store.state.products || [],
-            fieldInEditing: '',
-            editedProducts: {
-                nameEdited: '',
-                statusEdited: ''
-            }
         }
+    },
+    components: {
+        CustomerList,
+        ProductList
     },
     methods: {
         changeList(e) {
             this.selectedList = e.target.id;
-        },
-        editField(product) {
-            this.fieldInEditing = product.name
-        },
-        removeItem(product) {
-            const newProductList = this.productList.filter(item => item.name !== product.name);
-            this.productList = newProductList;
-        },
-        cancelEdit() {
-            this.fieldInEditing = ''
-        },
-        saveField(product) {
-            const newProductList = this.productList.map(item => {
-                if (item.name === product.name) {
-                    return {
-                        name: this.editedProducts.nameEdited,
-                        activatedStatus: this.editedProducts.statusEdited
-                    }
-                } else {
-                    return item
-                }
-            });
-
-            this.productList = newProductList;
-            this.fieldInEditing = '';
-        },
+        }
     }
 }
 </script>
@@ -113,7 +53,7 @@ export default {
     border-radius: 5px 5px 0 0;
 }
 
-.list-container .button-container button {
+.button-container button {
     background-color: darkgray;
     color: #ffffff;
     text-shadow: 1px 1px 5px #000000af;
@@ -123,77 +63,5 @@ export default {
     cursor: pointer;
     margin: 10px 25px 20px 10px;
     padding: 7px 10px;
-}
-
-.customer-list,
-.product-list {
-    border: 1px solid #1d1c1c8e;
-    border-radius: 3px;
-    padding: 15px;
-}
-
-.customer-list p,
-.product-list p {
-    font-size: 17px;
-}
-
-.list {
-    margin: 15px 0;
-}
-
-.list .item {
-    background-color: #ececec;
-    margin: 10px 0;
-    padding: 5px;
-    border-radius: 5px;
-}
-
-.list .item .content {
-    display: flex;
-    align-items: center;
-    flex-wrap: wrap;
-    gap: 18px;
-}
-
-.list .item .content p {
-    font-size: 15px;
-}
-
-.list .item .content label {
-    margin-right: 7px;
-}
-
-.list .item .content input {
-    padding: 5px;
-    border-radius: 3px;
-}
-
-.list .item .content button {
-    background-color: #7a7a7a;
-    color: #ffffff;
-    font-size: 14px;
-    cursor: pointer;
-    padding: 5px 6px;
-    border-radius: 3px;
-}
-
-.list .item .content.edit label {
-    margin-right: 7px;
-}
-
-.list .item .content.edit input,
-select {
-    border: 1px solid #1d1c1c8e;
-    border-radius: 3px;
-    padding: 3px;
-}
-
-.list .item .content.edit .btn-cancel {
-    background-color: #e91515;
-    margin-right: 10px;
-}
-
-.list .item .content.edit .btn-save {
-    background-color: #1ce915;
 }
 </style>
