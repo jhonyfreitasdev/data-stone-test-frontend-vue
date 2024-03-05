4930a2790f8d24672307073ec539a4f0f21e17a2
Jhony Freitas <jhony00._@hotmail.com>
2024-03-05 02:09:57 -0300

    Validando formulário de Clientes

--- src/components/form/customer-registration-form/CustomerRegistrationForm.vue
+++ src/components/form/customer-registration-form/CustomerRegistrationForm.vue
@@ -4,23 +4,24 @@
         <form @submit.prevent="submitForm">
             <div class="input-container">
                 <label for="customer-name">Nome completo</label>
-                <input v-model="name" type="text" name="name" id="customer-name">
+                <input v-model="name" type="text" name="name" id="customer-name" required>
             </div>
             <div class="input-container">
                 <label for="customer-document">CPF</label>
-                <input v-model="document" type="text" name="document" id="customer-document">
+                <input v-model="document" @change="onChangeDocument" type="text" name="document"
+                    id="customer-document" required>
             </div>
             <div class="input-container">
                 <label for="customer-tel">Telefone</label>
-                <input v-model="tel" type="tel" name="tel" id="customer-tel">
+                <input v-model="tel" type="tel" name="tel" id="customer-tel" required>
             </div>
             <div class="input-container">
                 <label for="customer-mail">Email</label>
-                <input v-model="mail" type="mail" name="mail" id="customer-mail">
+                <input v-model="mail" type="mail" name="mail" id="customer-mail" required>
             </div>
             <div class="input-container">
                 <label for="customer-status">Cliente ativo</label>
-                <select v-model="activatedStatus" name="status" id="customer-status">
+                <select v-model="activatedStatus" name="status" id="customer-status" required>
                     <option value="yes"> Sim </option>
                     <option value="no"> Não </option>
                 </select>
@@ -45,6 +46,17 @@ export default {
         }
     },
     methods: {
+        onChangeDocument() {
+            const foundCustomer = this.$store.state.customers.find(item => item.document === this.document)
+            if (foundCustomer !== undefined) {
+                window.alert('Cliente já cadastrado')
+                this.name = '';
+                this.document = '';
+                this.tel = '';
+                this.mail = '';
+                this.activatedStatus = null
+            };
+        },
         submitForm() {
             this.$store.commit('addCustomer', {
                 name: this.name,
@@ -55,6 +67,7 @@ export default {
                 associatedProducts: []
             });
 
+            window.alert('Cliente cadastrado com sucesso!')
             this.name = '';
             this.document = '';
             this.tel = '';
