5b22d1465ecee51d5ea8ac219a2957958fc58b49
Jhony Freitas <jhony00._@hotmail.com>
2024-03-05 02:10:07 -0300

    Validando formulário de produtos

--- src/components/form/product-registration-form/ProductRegistrationForm.vue
+++ src/components/form/product-registration-form/ProductRegistrationForm.vue
@@ -4,11 +4,11 @@
         <form @submit.prevent="submitForm">
             <div class="input-container">
                 <label for="product-name">Nome do produto</label>
-                <input v-model="name" id="product-name" name="name" type="text" />
+                <input v-model="name" @change="onChangeName()" id="product-name" name="name" type="text" required/>
             </div>
             <div class="input-container">
                 <label for="product-status">Produto ativo</label>
-                <select v-model="activatedStatus" name="status" id="product-status">
+                <select v-model="activatedStatus" name="status" id="product-status" required>
                     <option value="yes"> Sim </option>
                     <option value="no"> Não </option>
                 </select>
@@ -30,12 +30,21 @@ export default {
         };
     },
     methods: {
+        onChangeName() {
+            const foundProduct = this.$store.state.products.find(item => item.name === this.name)
+            if (foundProduct !== undefined) {
+                window.alert('Produto já cadastrado')
+                this.name = '';
+                this.activatedStatus = '';
+            };
+        },
         submitForm() {
             this.$store.commit('addProduct', {
                 name: this.name,
                 activatedStatus: this.activatedStatus === 'yes'
             });
 
+            window.alert('Produto cadastrado com sucesso!')
             this.name = '';
             this.activatedStatus = '';
         }
